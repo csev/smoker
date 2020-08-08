e97add43d1c18471cdb387cc5daeca11dd02764d
Charles Severance <csev@umich.edu>
2020-08-08 11:32:43 -0400

    Progress

---
+++ broken.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect('spider.sqlite')
+cur = conn.cursor()
+
+cur.execute('''SELECT Bad.url AS bad_url, Bad.size, Bad.code, Bad.content_type, Fr.url AS from_url
+     FROM Pages AS Bad
+     JOIN Links ON Bad.id = Links.to_id
+     JOIN Pages AS Fr ON Links.from_id = Fr.id
+     WHERE Bad.code <> 200''')
+
+count = 0
+for row in cur :
+    if count < 50 : print(row)
+    count = count + 1
+print(count, 'rows.')
+cur.close()
--- spider.py
+++ spider.py
@@ -4,6 +4,7 @@ import ssl
 from urllib.parse import urljoin
 from urllib.parse import urlparse
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 # Ignore SSL certificate errors
@@ -15,8 +16,8 @@ conn = sqlite3.connect('spider.sqlite')
 cur = conn.cursor()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS Pages
-    (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT,
-     error INTEGER, old_rank REAL, new_rank REAL)''')
+    (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT, size INTEGER,
+     code INTEGER, content_type TEXT, old_rank REAL, new_rank REAL)''')
 
 cur.execute('''CREATE TABLE IF NOT EXISTS Links
     (from_id INTEGER, to_id INTEGER, UNIQUE(from_id, to_id))''')
@@ -24,13 +25,13 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Links
 cur.execute('''CREATE TABLE IF NOT EXISTS Webs (url TEXT UNIQUE)''')
 
 # Check to see if we are already in progress...
-cur.execute('SELECT id,url FROM Pages WHERE html is NULL and error is NULL ORDER BY RANDOM() LIMIT 1')
+cur.execute('SELECT id,url FROM Pages WHERE html is NULL and code is NULL ORDER BY RANDOM() LIMIT 1')
 row = cur.fetchone()
 if row is not None:
     print("Restarting existing crawl.  Remove spider.sqlite to start a fresh crawl.")
 else :
     starturl = input('Enter web url or enter: ')
-    if ( len(starturl) < 1 ) : starturl = 'http://www.dr-chuck.com/'
+    if ( len(starturl) < 1 ) : starturl = 'http://localhost:8888/py4e'
     if ( starturl.endswith('/') ) : starturl = starturl[:-1]
     web = starturl
     if ( starturl.endswith('.htm') or starturl.endswith('.html') ) :
@@ -58,7 +59,7 @@ while True:
         many = int(sval)
     many = many - 1
 
-    cur.execute('SELECT id,url FROM Pages WHERE html is NULL and error is NULL ORDER BY RANDOM() LIMIT 1')
+    cur.execute('SELECT id,url FROM Pages WHERE html is NULL and code is NULL ORDER BY RANDOM() LIMIT 1')
     try:
         row = cur.fetchone()
         # print row
@@ -70,20 +71,22 @@ while True:
         break
 
     print(fromid, url, end=' ')
+    actualurl = url
 
     # If we are retrieving this page, there should be no links from it
     cur.execute('DELETE from Links WHERE from_id=?', (fromid, ) )
     try:
         document = urlopen(url, context=ctx)
+        actualurl = document.geturl()
 
         html = document.read()
-        if document.getcode() != 200 :
-            print("Error on page: ",document.getcode())
-            cur.execute('UPDATE Pages SET error=? WHERE url=?', (document.getcode(), url) )
+        info = document.info()
+
+        cur.execute('UPDATE Pages SET code=?, size=?, content_type=? WHERE url=?',
+                (document.getcode(), len(html), info.get_content_type(), url) )
 
         if 'text/html' != document.info().get_content_type() :
-            print("Ignore non text/html page")
-            cur.execute('DELETE FROM Pages WHERE url=?', ( url, ) )
+            print("Note non text/html page")
             conn.commit()
             continue
 
@@ -94,9 +97,16 @@ while True:
         print('')
         print('Program interrupted by user...')
         break
-    except:
+    except HTTPError as exc:
+        print("HTTPError")
+        print(exc)
+        cur.execute('UPDATE Pages SET code=? WHERE url=?', (exc.code, url) )
+        conn.commit()
+        continue
+    except Exception as exc:
         print("Unable to retrieve or parse page")
-        cur.execute('UPDATE Pages SET error=-1 WHERE url=?', (url, ) )
+        print(exc)
+        cur.execute('UPDATE Pages SET code=-1 WHERE url=?', (url, ) )
         conn.commit()
         continue
 
@@ -109,25 +119,45 @@ while True:
     count = 0
     for tag in tags:
         href = tag.get('href', None)
+        xhref = href
         if ( href is None ) : continue
-        # Resolve relative references like href="/contact"
+
+        # hrefs look like
+        # - global with http or https
+        # - relative and absolute like /abc
+        # - relative without a slash
+        # Current url may or may not end in a slash
         up = urlparse(href)
-        if ( len(up.scheme) < 1 ) :
-            href = urljoin(url, href)
+        aup = urlparse(actualurl)
+        actualroot = aup.scheme + aup.netloc # No slash
+        # print(href, up)
+        # print(actualurl, actualroot, aup)
+        if ( len(up.scheme) > 1 ) :
+            pass  # Global href
+        elif href.startswith("/") :
+            href = urljoin(actualroot, href)
+        elif actualurl.endswith("/") : # in same folder
+            href = urljoin(actualurl, href)
+        else : # Need to trim off the last path bit
+            pos = actualurl.rindex('/')
+            if pos > 0 : 
+                href = actualurl[:pos+1] + href
+            else : # Hopefully never
+                print("WTF ??? ",actualurl,href)
+
         ipos = href.find('#')
         if ( ipos > 1 ) : href = href[:ipos]
-        if ( href.endswith('.png') or href.endswith('.jpg') or href.endswith('.gif') ) : continue
         if ( href.endswith('/') ) : href = href[:-1]
-        # print href
         if ( len(href) < 1 ) : continue
 
-		# Check if the URL is in any of the webs
+	# Check if the URL is in any of the webs
         found = False
         for web in webs:
             if ( href.startswith(web) ) :
                 found = True
                 break
         if not found : continue
+        # print(actualurl, xhref, href)
 
         cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES ( ?, NULL, 1.0 )', ( href, ) )
         count = count + 1
