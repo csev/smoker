b2a2448fe64e5794c75089bdc0b1d7590e77aaf8
Charles Severance <csev@umich.edu>
2020-11-02 10:09:01 -0500

    Add some documentation.

--- sakai-smoker.py
+++ sakai-smoker.py
@@ -91,3 +91,62 @@ if __name__ == "__main__":
     app = SakaiSmoker(base, 'smoker.sqlite', walk)
     app.run(many);
 
+""" 
+root@smoker:/var/www/html# cat aa_readme.txt
+Smoker Test Server
+------------------
+
+This runs the smoker test on several Sakai servers each night.
+
+https://github.com/csev/smoker
+
+This page shows the results of each run and keeps results of the previous two runs.
+It also calcualtes any changes between the previous run and the most recent run (deltas).
+
+For example, here are the files:
+
+    nightly-mysql-0-delta.txt   Errors in the most recent run that were not in the previous run
+    nightly-mysql-0-latest.txt	Errors in the most recent run
+    nightly-mysql-0-log.txt	Log of the most recent run
+    nightly-mysql-1-prev.txt	Errors from the previous run (usually 24 hours earlier)
+    nightly-mysql-2-prev.txt	Errors from the run before that (usually 48 hours earlier)
+
+For each entry it looks like:
+
+https://trunk-mysql.nightly.sakaiproject.org/portal/help/TOCDisplay/content.hlp?docId=
+Linked from:
+https://trunk-mysql.nightly.sakaiproject.org/portal/help/TOCDisplay/content.hlp?docId=accessibilityinformation
+454 1123 text/html;charset=ISO-8859-1
+
+This shows that the first link returned 1123 characters and got a "454" error.  The second
+link is the page where we found the "broken link".
+
+To reproduce, go the server and login as "admin" and visit the second (source) link.  Find the
+reference to the first link (often in the source) and click on it.
+
+Special Error Codes
+-------------------
+
+Some of the errors lik 404 and 500 mean what they normally mean - but smoker does some analysis of the text page
+looking for sakai specific errors.
+
+404 - Not Found
+500 - Server error
+
+450 - NullPointerException
+453 - The string "To send a bug report,..." was found on the page
+454 - The string "Apache Tomcat" was found somewhere in the page
+455 - The string "Blank tool content" was found - usually this is a velocity error
+
+You can see the extra page processing at:
+
+https://github.com/csev/smoker/blob/master/sakai-smoker.py#L49
+
+-----
+
+Questions welcome.
+
+/Chuck
+
+root@smoker:/var/www/html#
+"""
